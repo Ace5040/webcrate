7c458e9d2422db993c1dddd9b976f56a4f2e5917
ace5040 <aleksandr.kalinovskiy@gmail.com>
2019-10-31 16:19:01 +0300

    suport config for exim primary name

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+sed -i "s/# primary_hostname =/primary_hostname = $EXIM_PRIMARY_HOSTNAME/g" /etc/mail/exim.conf
+
 /sitesbox/generate_configs.sh
 /sitesbox/generate_hosts.sh
 dnsmasq --server=$SITESBOX_EXTERNAL_DNS_IP -d &

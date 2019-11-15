1f80a974f94fda4c91c8dfe250c5eb8e27934065
ace5040 <aleksandr.kalinovskiy@gmail.com>
2019-11-15 16:25:12 +0300

    external exim.conf

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sed -i "s/# primary_hostname =/primary_hostname = $EXIM_PRIMARY_HOSTNAME/g" /etc/mail/exim.conf
+#sed -i "s/# primary_hostname =/primary_hostname = $EXIM_PRIMARY_HOSTNAME/g" /etc/mail/exim.conf
 
 /sitesbox/generate_configs.sh
 /sitesbox/generate_hosts.sh

73457cf65c7b38aaf776a78c266f97d2af5cc029
ace5040 <aleksandr.kalinovskiy@gmail.com>
2020-02-23 08:24:27 +0300

    fix nginx group generation

--- src/nginx/generate_configs.sh
+++ src/nginx/generate_configs.sh
@@ -4,7 +4,7 @@ if [[ $SITESBOX_MODE == "PRODUCTION" ]]; then
 
     for group_data in $(cut -d: -f1,3 /sitesbox/group); do
         g=`cut -d: -f1 <<< $group_data`;
-        if [[ -d "$SITES_PATH/$g" ]]; then
+        if [[ -d "/sites/$g" ]]; then
           gid=`cut -d: -f2 <<< $group_data`;
           g_exist=`grep -c "^$g:" /etc/group`;
           if [[ $g_exist == 0 ]]; then

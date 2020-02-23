9364987df8bd2eba6a3e203d01a2043f773094ab
ace5040 <aleksandr.kalinovskiy@gmail.com>
2020-02-24 00:30:56 +0300

    auto cd after ssh login

--- src/sitesbox/sitesbox_profile.sh
+++ src/sitesbox/sitesbox_profile.sh
@@ -2,3 +2,7 @@ alias ls='ls --color=auto'
 export DRUSH_LAUNCHER_FALLBACK=~/.composer/vendor/bin/drush
 export VISUAL=mcedit
 PATH=/sitesbox/bin:$PATH
+u=${HOME//\/sites\/}
+[ -d "$HOME/$u.test" ] && cd ~/$u.test;
+[ -d "$HOME/data" ] && cd ~/data;
+[ -d "$HOME/app" ] && cd ~/app;

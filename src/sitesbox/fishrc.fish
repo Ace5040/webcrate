183e93ff28011583bcd9e55a9643d826f3ba2c64
ace5040 <aleksandr.kalinovskiy@gmail.com>
2020-03-15 07:26:33 +0300

    fish theme

--- src/sitesbox/fishrc.fish
+++ src/sitesbox/fishrc.fish
@@ -2,6 +2,7 @@ set VISUAL mcedit
 set PATH /sitesbox/bin $PATH
 set DRUSH_LAUNCHER_FALLBACK ~/.composer/vendor/bin/drush
 set u (basename $PWD)
+set fish_greeting ""
 if test -d $HOME/$u.test
     cd ~/$u.test
 end

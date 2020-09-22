3f865606ffe1f4d0fc88eae646c8598ae206443b
ace5040 <aleksandr.kalinovskiy@gmail.com>
2020-09-22 04:55:09 +0300

    fix data folder parse

--- src/webcrate-core/bashrc.sh
+++ src/webcrate-core/bashrc.sh
@@ -3,7 +3,7 @@ export VISUAL=mcedit
 export DRUSH_LAUNCHER_FALLBACK=~/.composer/vendor/bin/drush
 PATH=/webcrate/bin:$PATH
 u=${HOME//\/sites\/}
-export DATA_FOLDER=`cat /webcrate/users.yml | awk "/${u}/,/backup/" | grep -oP "(?<=root_folder: ).*" | grep -oP ".*?(?=/)" | head -c -1`
+export DATA_FOLDER=`cat /webcrate/users.yml | awk "/${u}/,/backup/" | grep -oP "(?<=root_folder: ).*" | cut -d "/" -f1 | head -c -1`
 [ -d "$HOME/$DATA_FOLDER" ] && cd ~/$DATA_FOLDER;
 [ -d "$HOME/$DATA_FOLDER/bin" ] && PATH=$HOME/$DATA_FOLDER/bin:$PATH;
 [ -r ~/phpversion.sh ] && . ~/phpversion.sh

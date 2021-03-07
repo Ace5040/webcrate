4342a91c5bfeb622ba4e20a13b62333259e945d0
ace5040 <aleksandr.kalinovskiy@gmail.com>
2021-03-07 21:53:48 +0300

    change config.sh file check

--- src/webcrate-core/bashrc.sh
+++ src/webcrate-core/bashrc.sh
@@ -3,7 +3,9 @@ export EDITOR=mcedit
 export VISUAL=mcedit
 export DRUSH_LAUNCHER_FALLBACK=~/.composer/vendor/bin/drush
 PATH=/webcrate-bin:$PATH
-[ -r ~/config.sh ] && . ~/config.sh
+if test -f ~/config.sh; then
+    source ~/config.sh
+fi
 [ -d "$HOME/$DATA_FOLDER" ] && cd ~/$DATA_FOLDER;
 [ -d "$HOME/$DATA_FOLDER/bin" ] && PATH=$HOME/$DATA_FOLDER/bin:$PATH;
 alias ls='ls --color=auto'

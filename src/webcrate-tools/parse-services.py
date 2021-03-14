f2a8d5474759b5059f8fd5dcb549f50363531ba1
ace5040 <aleksandr.kalinovskiy@gmail.com>
2021-03-14 22:05:25 +0300

    options nginx services

--- src/webcrate-tools/parse-services.py
+++ src/webcrate-tools/parse-services.py
@@ -13,56 +13,80 @@ DOCKER_HOST_IP = os.environ.get('DOCKER_HOST_IP', '')
 WEBCRATE_UID = os.environ.get('WEBCRATE_UID', '1000')
 WEBCRATE_GID = os.environ.get('WEBCRATE_GID', '1000')
 
+
+WEBCRATE_SERVICE_HTMLTOPDF = os.environ.get('WEBCRATE_SERVICE_HTMLTOPDF', 'false') == 'true'
+WEBCRATE_SERVICE_DOCTOHTML = os.environ.get('WEBCRATE_SERVICE_DOCTOHTML', 'false') == 'true'
+WEBCRATE_SERVICE_SOLR = os.environ.get('WEBCRATE_SERVICE_SOLR', 'false') == 'true'
+WEBCRATE_SERVICE_SYNAPSE = os.environ.get('WEBCRATE_SERVICE_SYNAPSE', 'false') == 'true'
+WEBCRATE_SERVICE_STATS = os.environ.get('WEBCRATE_SERVICE_STATS', 'false') == 'true'
+
+print(WEBCRATE_SERVICE_HTMLTOPDF)
+print(WEBCRATE_SERVICE_STATS)
+
 for servicename, service in services.items():
-  service.name = servicename
-  template = 'default';
-  if os.path.isfile(f'/webcrate/nginx-templates/{service.name}-service.conf'):
-    template = service.name
-  os.system(f'cp -rf /webcrate/nginx-templates/{template}-service.conf /webcrate/nginx_configs/{service.name}-service.conf')
+  if  ( servicename != 'synapse' or WEBCRATE_SERVICE_SYNAPSE  ) and \
+      ( servicename != 'synapse-admin' or WEBCRATE_SERVICE_SYNAPSE ) and \
+      ( servicename != 'doctohtml' or WEBCRATE_SERVICE_DOCTOHTML ) and \
+      ( servicename != 'htmltopdf' or WEBCRATE_SERVICE_HTMLTOPDF ) and \
+      ( servicename != 'grafana' or WEBCRATE_SERVICE_STATS ) and \
+      ( servicename != 'solr' or WEBCRATE_SERVICE_SOLR ):
 
-  with open(f'/webcrate/nginx_configs/{service.name}-service.conf', 'r') as f:
-    conf = f.read()
-    f.close()
+        service.name = servicename
 
-  conf = conf.replace('%domain%', service.domain)
-  conf = conf.replace('%host%', service.name)
-  conf = conf.replace('%service%', service.name)
-  conf = conf.replace('%port%', str(service.port))
+        template = 'default';
+        if os.path.isfile(f'/webcrate/nginx-templates/{service.name}-service.conf'):
+          template = service.name
+        os.system(f'cp -rf /webcrate/nginx-templates/{template}-service.conf /webcrate/nginx_configs/{service.name}-service.conf')
 
-  with open(f'/webcrate/nginx_configs/{service.name}-service.conf', 'w') as f:
-    f.write(conf)
-    f.close()
+        with open(f'/webcrate/nginx_configs/{service.name}-service.conf', 'r') as f:
+          conf = f.read()
+          f.close()
 
-  print(f'{service.name} config - generated')
+        conf = conf.replace('%domain%', service.domain)
+        conf = conf.replace('%host%', service.name)
+        conf = conf.replace('%service%', service.name)
+        conf = conf.replace('%port%', str(service.port))
 
-  if service.https == 'letsencrypt':
-    if os.path.isdir(f'/webcrate/letsencrypt/live/{service.name}'):
-      with open(f'/webcrate/ssl.conf', 'r') as f:
-        conf = f.read()
-        f.close()
-      conf = conf.replace('%type%', 'letsencrypt')
-      conf = conf.replace('%path%', f'live/{service.name}')
-      with open(f'/webcrate/ssl_configs/{service.name}.conf', 'w') as f:
-        f.write(conf)
-        f.close()
-      print(f'ssl config for {service.name} - generated')
-  if service.https == 'openssl':
-    if os.path.isdir(f'/webcrate/openssl/{service.name}'):
-      with open(f'/webcrate/ssl.conf', 'r') as f:
-        conf = f.read()
-        f.close()
-      conf = conf.replace('%type%', 'openssl')
-      conf = conf.replace('%path%', service.name)
-      with open(f'/webcrate/ssl_configs/{service.name}.conf', 'w') as f:
-        f.write(conf)
-        f.close()
-      print(f'ssl config for {service.name} - generated')
+        with open(f'/webcrate/nginx_configs/{service.name}-service.conf', 'w') as f:
+          f.write(conf)
+          f.close()
+
+        print(f'{service.name} config - generated')
+
+        if service.https == 'letsencrypt':
+          if os.path.isdir(f'/webcrate/letsencrypt/live/{service.name}'):
+            with open(f'/webcrate/ssl.conf', 'r') as f:
+              conf = f.read()
+              f.close()
+            conf = conf.replace('%type%', 'letsencrypt')
+            conf = conf.replace('%path%', f'live/{service.name}')
+            with open(f'/webcrate/ssl_configs/{service.name}.conf', 'w') as f:
+              f.write(conf)
+              f.close()
+            print(f'ssl config for {service.name} - generated')
+        if service.https == 'openssl':
+          if os.path.isdir(f'/webcrate/openssl/{service.name}'):
+            with open(f'/webcrate/ssl.conf', 'r') as f:
+              conf = f.read()
+              f.close()
+            conf = conf.replace('%type%', 'openssl')
+            conf = conf.replace('%path%', service.name)
+            with open(f'/webcrate/ssl_configs/{service.name}.conf', 'w') as f:
+              f.write(conf)
+              f.close()
+            print(f'ssl config for {service.name} - generated')
 
 if WEBCRATE_MODE == "DEV":
   with open(f'/webcrate-dnsmasq/config/hosts_nginx', 'a') as f:
     for servicename, service in services.items():
-      service.name = servicename
-      f.write(f'{DOCKER_HOST_IP} {service.domain}\n')
+      if  ( servicename != 'synapse' or WEBCRATE_SERVICE_SYNAPSE ) and \
+          ( servicename != 'synapse-admin' or WEBCRATE_SERVICE_SYNAPSE ) and \
+          ( servicename != 'doctohtml' or WEBCRATE_SERVICE_DOCTOHTML ) and \
+          ( servicename != 'htmltopdf' or WEBCRATE_SERVICE_HTMLTOPDF ) and \
+          ( servicename != 'grafana' or WEBCRATE_SERVICE_STATS ) and \
+          ( servicename != 'solr' or WEBCRATE_SERVICE_SOLR ):
+            service.name = servicename
+            f.write(f'{DOCKER_HOST_IP} {service.domain}\n')
     f.close()
 
 os.system(f'chown -R {WEBCRATE_UID}:{WEBCRATE_GID} /webcrate/nginx_configs')

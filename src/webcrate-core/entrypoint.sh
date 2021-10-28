584f99497077c9d9fdf44cacacf8c2f1be09446b
ace5040 <aleksandr.kalinovskiy@gmail.com>
2021-10-28 23:40:39 +0300

    allow sshd pubkey auth

--- src/webcrate-core/entrypoint.sh
+++ src/webcrate-core/entrypoint.sh
@@ -7,6 +7,4 @@ chown root:root /etc/mail/exim.conf
 /webcrate/sync_ssh_keys.sh
 /webcrate/parse-projects.py
 echo server=$WEBCRATE_EXTERNAL_DNS_IP >> /etc/dnsmasq.conf
-sed -i "s/Type=dbus/Type=simple/g" /usr/lib/systemd/system/dnsmasq.service
-sed -i "s/--enable-dbus //g" /usr/lib/systemd/system/dnsmasq.service
 exec systemctl init

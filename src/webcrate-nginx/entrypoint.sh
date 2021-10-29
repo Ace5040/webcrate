6ba4e5bca20eda5883fd3c818bad7b44d04d6f6c
ace5040 <aleksandr.kalinovskiy@gmail.com>
2021-10-29 03:00:04 +0300

    fix nginx config

--- src/webcrate-nginx/entrypoint.sh
+++ src/webcrate-nginx/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s/\/var\/log\/nginx\/error.log/\/dev\/null/g" /etc/nginx/nginx.conf
-sed -i "s/\/var\/log\/nginx\/access.log/off/g" /etc/nginx/nginx.conf
+sed -i "s/error_log  \/var\/log\/nginx\/error.log notice/error_log  \/dev\/null crit/g" /etc/nginx/nginx.conf
+sed -i "s/access_log  \/var\/log\/nginx\/access.log  main/access_log  off/g" /etc/nginx/nginx.conf
 sed -i "s/#gzip  on/include \/webcrate\/nginx-config\/\*\.conf/g" /etc/nginx/nginx.conf
 /webcrate/parse-projects.py
 

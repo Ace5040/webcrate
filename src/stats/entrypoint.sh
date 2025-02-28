edb5ac4023e9fb72cf67884863014d0a9aa5ec2e
ace5040 <aleksandr.kalinovskiy@gmail.com>
2025-03-01 02:28:39 +0300

    stats debian

--- src/stats/entrypoint.sh
+++ src/stats/entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 env | sed 's/^\(.*\)$/export \1/g' > /docker.env
-exec systemctl init
+cp /supervisord.conf.template /etc/supervisord.conf
+/usr/bin/supervisord -c /etc/supervisord.conf

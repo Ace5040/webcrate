3be305a03081c53f784462b3eb1c60184338bf0a
ace5040 <aleksandr.kalinovskiy@gmail.com>
2026-04-06 20:53:51 +0300

    phpmyadmin hosts list dynamic update

--- config/phpmyadmin/config.user.inc.php
+++ config/phpmyadmin/config.user.inc.php
@@ -1,6 +1,13 @@
 <?php
 
-for ($i = 1; isset($hosts[$i - 1]); $i++) {
-    $cfg['Servers'][$i]['hide_db'] = 'information_schema|performance_schema|mysql';
-}
+$hostsFile = '/webcrate/meta/dbhosts.txt';
+$hosts = file_exists($hostsFile)
+    ? array_filter(array_map('trim', explode(',', file_get_contents($hostsFile))))
+    : [];
 
+foreach ($hosts as $i => $host) {
+    $idx = $i + 1;
+    $cfg['Servers'][$idx]['host'] = $host;
+    $cfg['Servers'][$idx]['auth_type'] = 'cookie';
+    $cfg['Servers'][$idx]['hide_db'] = 'information_schema|performance_schema|mysql';
+}
